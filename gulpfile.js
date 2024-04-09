0e0f477ab559ee8df8c770d620562d4500dfa36a
Aram <105285565+aram-ninja@users.noreply.github.com>
2024-04-09 18:02:44 +0400

    third commit

--- gulpfile.js
+++ gulpfile.js
@@ -38,3 +38,23 @@ function watchTask() {
   watch('./assets/js/main.js', jsMin);
 }
 exports.default = series(compilerSass, jsMin, watchTask);
+
+
+
+
+$(document).ready(function() {
+  $("#color_mode").on("change", function () {
+      colorModePreview(this);
+  })
+});
+
+function colorModePreview(ele) {
+  if($(ele).prop("checked") == true){
+      $('body').addClass('dark-preview');
+      $('body').removeClass('white-preview');
+  }
+  else if($(ele).prop("checked") == false){
+      $('body').addClass('white-preview');
+      $('body').removeClass('dark-preview');
+  }
+}
